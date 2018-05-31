bac635b8be646660dae9652fb1305cc02b1e55b4
Matt Collins <5278184+TheMattCollins0@users.noreply.github.com>
2018-05-31 18:13:51 +0100

    Update CommitVersionChange.ps1

--- Tools/CommitVersionChange.ps1
+++ Tools/CommitVersionChange.ps1
@@ -10,11 +10,7 @@ $ReadMeUpdatePath = ".\" + "ReadMe" + ".md"
 git config user.email "matt.collins@node-it.com"
 git config user.name "TheMattCollins0"
 git checkout master
-git add "$ModulePath"
-git add "$PesterResultsPath"
-git add "$PSSAResultsPath"
-git add "$CodeCoverageResultsPath"
-git add "$ReadMeUpdatePath"
+git add .
 git commit -m "Updated Version Number ***NO_CI***"
 git push origin HEAD:master
 
