0e59960cb3e3f5517c40ff6568465ffb748e8aad
TheMattCollins0 <matt.collins@node-it.com>
2018-06-04 13:51:53 +0100

    Change to Invoke-Build.ps1

--- Tools/Invoke-Build.ps1
+++ Tools/Invoke-Build.ps1
@@ -28,7 +28,7 @@ $ErrorActionPreference = 'stop'
 # Install the Nuget package provider, Pester and PSScriptAnalyzer packages for testing
 Install-PackageProvider -Name Nuget -Scope CurrentUser -Force -Confirm:$false
 Install-Module -Name Pester -Scope CurrentUser -Force -SkipPublisherCheck -Confirm:$false
-Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force -Confirm:$false
+# Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force -Confirm:$false
 Install-Module -Name PlatyPS -Scope CurrentUser -Force -Confirm:$false
 
 # Install third party modules required for the module 
