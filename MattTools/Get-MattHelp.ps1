08a61eb5559764c8e23463d89017319c91d4488f
Matt Collins <matt.collins@node-it.com>
2018-06-12 09:35:21 +0100

    Replacement of Write-Host with Write-Information in Get-MattHelp

--- MattTools/Get-MattHelp.ps1
+++ MattTools/Get-MattHelp.ps1
@@ -12,34 +12,34 @@ function Get-MattHelp {
     [CmdletBinding()]
     param ()
 
-    Write-Host ""
-    Write-Host "Type Connect-EOnline to connect to a clients Office 365 or Azure AD"
-    Write-Host "You need to supply Office 365 Global Admin credentials for each client you want to connect to"
-    Write-Host ""
-    Write-Host "Type Connect-Client to open a GUI showing a list of clients you can connect to Office 365 as"
-    Write-Host "If you know the name of the client, you can type Connect-Client -ClientName ClientName or Connect-Client -Name ClientName"
-    Write-Host ""
-    Write-Host "Type Exit-EOnline to disconnect from Office 365 and close the PowerShell console"
-    Write-Host ""
-    Write-Host ""
-    Write-Host "Commonly used commands:"
-    Write-Host "UserA is always the sharing user. UserB is always the user requesting access"
-    Write-Host "Always try to supply the users UserPrincipalName in any commands"
-    Write-Host ""
-    Write-Host "Add full access to mailbox without automapping"
-    Write-Host 'Add-MailboxPermission -Identity "UserA" -User "UserB" -AccessRights FullAccess -AutoMapping:$false'
-    Write-Host ""
-    Write-Host "Set single users password to never expire"
-    Write-Host 'Set-MsolUser -UserPrincipalName "" -PasswordNeverExpires:$true'
-    Write-Host ""
-    Write-Host "Assign calendar permissions to a user"
-    Write-Host 'Add-MailboxFolderPermission -Identity "UserA:\Calendar" -AccessRights PublishingEditor -User "UserB"'
-    Write-Host ""
-    Write-Host "List the number of Office 365 licences in use"
-    Write-Host "Get-MSOLAccountSku"
-    Write-Host ""
-    Write-Host "Update my Node Office365Connect PowerShell module"
-    Write-Host 'Install-Module -Repository NodePowerShellRepository Office365Connect -Verbose -Scope CurrentUser -Force'
-    Write-Host ""
+    Write-Information ""
+    Write-Information "Type Connect-EOnline to connect to a clients Office 365 or Azure AD"
+    Write-Information "You need to supply Office 365 Global Admin credentials for each client you want to connect to"
+    Write-Information ""
+    Write-Information "Type Connect-Client to open a GUI showing a list of clients you can connect to Office 365 as"
+    Write-Information "If you know the name of the client, you can type Connect-Client -ClientName ClientName or Connect-Client -Name ClientName"
+    Write-Information ""
+    Write-Information "Type Exit-EOnline to disconnect from Office 365 and close the PowerShell console"
+    Write-Information ""
+    Write-Information ""
+    Write-Information "Commonly used commands:"
+    Write-Information "UserA is always the sharing user. UserB is always the user requesting access"
+    Write-Information "Always try to supply the users UserPrincipalName in any commands"
+    Write-Information ""
+    Write-Information "Add full access to mailbox without automapping"
+    Write-Information 'Add-MailboxPermission -Identity "UserA" -User "UserB" -AccessRights FullAccess -AutoMapping:$false'
+    Write-Information ""
+    Write-Information "Set single users password to never expire"
+    Write-Information 'Set-MsolUser -UserPrincipalName "" -PasswordNeverExpires:$true'
+    Write-Information ""
+    Write-Information "Assign calendar permissions to a user"
+    Write-Information 'Add-MailboxFolderPermission -Identity "UserA:\Calendar" -AccessRights PublishingEditor -User "UserB"'
+    Write-Information ""
+    Write-Information "List the number of Office 365 licences in use"
+    Write-Information "Get-MSOLAccountSku"
+    Write-Information ""
+    Write-Information "Update my Node Office365Connect PowerShell module"
+    Write-Information 'Install-Module -Repository NodePowerShellRepository Office365Connect -Verbose -Scope CurrentUser -Force'
+    Write-Information ""
 
 }
