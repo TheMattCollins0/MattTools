01cdcf2daf4eb8e0c0a99daaa6006043c28f0162
Matt Collins <matt.collins@node-it.com>
2018-06-13 14:44:47 +0100

    Change to the Set-LocationGitHub function

--- MattTools/Set-LocationGitHub.ps1
+++ MattTools/Set-LocationGitHub.ps1
@@ -4,13 +4,15 @@ function Set-LocationGitHub {
     .SYNOPSIS
     Set location to GitHub
     .DESCRIPTION
-    Function to check if my GitHub path exists then set the prompts location to the path. Function can also be called by typing Gh
+    Function to check if my GitHub path exists then set the prompts location to the path. Function can also be called by typing GitHub or Gh
     .PARAMETER GitHub
     The -GitHub parameter allows you to supply the path to your GitHub folder. If the folder does not exist, you will see an error
     .EXAMPLE
     Set-LocationGitHub
     .EXAMPLE
     Gh
+    .EXAMPLE
+    GitHub
     #>
 
     [CmdletBinding(SupportsShouldProcess=$True)]
@@ -24,7 +26,6 @@ function Set-LocationGitHub {
     )
 
     if ($PSCmdlet.ShouldProcess("Change of location to the $GitHub path successful")) {     
-
         # Test if the $GitHub path is valid
         if (Test-Path "$GitHub") {
             # Set console location to the GitHub drive
@@ -38,3 +39,4 @@ function Set-LocationGitHub {
 }
 
 New-Alias -Name Gh -Value Set-LocationGitHub
+New-Alias -Name GitHub -Value Set-LocationGitHub
