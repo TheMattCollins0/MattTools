5a7001028b5ccd7cb485dc781478745f6e9b1330
Matt Collins <matt.collins@node-it.com>
2018-06-13 14:56:09 +0100

    Change to the CommitVersionChange script

--- Tools/CommitVersionChange.ps1
+++ Tools/CommitVersionChange.ps1
@@ -9,7 +9,7 @@ $ReadMeUpdatePath = ".\" + "ReadMe" + ".md"
 # Git for the version number change commit back to GitHub
 git config user.email "matt.collins@node-it.com"
 git config user.name "TheMattCollins0"
-git checkout master
+# git checkout master
 git add "$ModulePath"
 git add "$PesterResultsPath"
 git add "$PSSAResultsPath"
@@ -17,4 +17,3 @@ git add "$CodeCoverageResultsPath"
 git add "$ReadMeUpdatePath"
 git commit -m "Updated Version Number ***NO_CI***"
 git push origin HEAD:master
-
