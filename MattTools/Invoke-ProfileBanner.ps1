d72be64e6fb4ad223a4bc1187a3eabd9aaec9ee8
TheMattCollins0 <Matt.Collins@node-it.com>
2018-07-03 13:31:58 +0100

    Changed Invoke-ProfileBanner time to red

--- MattTools/Invoke-ProfileBanner.ps1
+++ MattTools/Invoke-ProfileBanner.ps1
@@ -9,19 +9,21 @@ function Invoke-ProfileBanner {
     Invoke-ProfileBanner
     #>
 
-    Write-Host " **************************************************************************************************************************************************** "
-    Write-Host " *                                                                                                                                                  * "
-    Write-Host " *                                                    Type Get-MattHelp to view help information                                                    * "
-    Write-Host " *                                                                                                                                                  * "
-    Write-Host ' *                                                     Type "Gh" to change to the GitHub folder                                                     * '
-    Write-Host ' *                                                   Type "Ph" to change to the PowerShell folder                                                   * '
-    Write-Host " *                                                                                                                                                  * "
-    Write-Host " *                     Use Invoke-MattPlaster to create a Plaster Template, follow prompts for ModuleName and ModuleDescription                     * "
-    Write-Host " *                                                                                                                                                  * "
-    Write-Host " *                          Install-Module -Repository NodePowerShellRepository -Name ModuleName -Scope CurrentUser -Force                          * "
-    Write-Host " *                                                                                                                                                  * "
-    Write-Host " *                                                             Session started at $Time                                                          * "
-    Write-Host " *                                                                                                                                                  * "
-    Write-Host " **************************************************************************************************************************************************** "
+    Write-Information " **************************************************************************************************************************************************** "
+    Write-Information " *                                                                                                                                                  * "
+    Write-Information " *                                                    Type Get-MattHelp to view help information                                                    * "
+    Write-Information " *                                                                                                                                                  * "
+    Write-Information ' *                                                     Type "Gh" to change to the GitHub folder                                                     * '
+    Write-Information ' *                                                   Type "Ph" to change to the PowerShell folder                                                   * '
+    Write-Information " *                                                                                                                                                  * "
+    Write-Information " *                     Use Invoke-MattPlaster to create a Plaster Template, follow prompts for ModuleName and ModuleDescription                     * "
+    Write-Information " *                                                                                                                                                  * "
+    Write-Information " *                          Install-Module -Repository NodePowerShellRepository -Name ModuleName -Scope CurrentUser -Force                          * "
+    Write-Information " *                                                                                                                                                  * "
+    Write-Information " *                                                             "
+    Write-Information "Session started at $Time" -NoNewLine -ForegroundColor Red
+    Write-Information "                                                          * " -NoNewLine
+    Write-Information " *                                                                                                                                                  * "
+    Write-Information " **************************************************************************************************************************************************** "
 
 }
