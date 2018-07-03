c1c41d7a72677d5cf1105aa21ca109fe833f18ea
TheMattCollins0 <Matt.Collins@node-it.com>
2018-07-03 13:15:24 +0100

    Created Invoke-ProfileBanner to show my profile banner

---
+++ MattTools/Invoke-ProfileBanner.ps1
@@ -0,0 +1,27 @@
+function Invoke-ProfileBanner {
+
+    <#
+    .SYNOPSIS
+    Show my PowerShell Profile banner
+    .DESCRIPTION
+    This function is purely used to write my PowerShell profile banner when I first open PowerShell
+    .EXAMPLE
+    Invoke-ProfileBanner
+    #>
+
+    Write-Host " **************************************************************************************************************************************************** "
+    Write-Host " *                                                                                                                                                  * "
+    Write-Host " *                                                    Type Get-MattHelp to view help information                                                    * "
+    Write-Host " *                                                                                                                                                  * "
+    Write-Host ' *                                                     Type "Gh" to change to the GitHub folder                                                     * '
+    Write-Host ' *                                                   Type "Ph" to change to the PowerShell folder                                                   * '
+    Write-Host " *                                                                                                                                                  * "
+    Write-Host " *                     Use Invoke-MattPlaster to create a Plaster Template, follow prompts for ModuleName and ModuleDescription                     * "
+    Write-Host " *                                                                                                                                                  * "
+    Write-Host " *                          Install-Module -Repository NodePowerShellRepository -Name ModuleName -Scope CurrentUser -Force                          * "
+    Write-Host " *                                                                                                                                                  * "
+    Write-Host " *                                                             Session started at $Time                                                          * "
+    Write-Host " *                                                                                                                                                  * "
+    Write-Host " **************************************************************************************************************************************************** "
+
+}
