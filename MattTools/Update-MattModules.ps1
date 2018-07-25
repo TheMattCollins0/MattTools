356a2bcd4678600842f001623828aa6bf33e3750
Matt Collins <Matt.Collins@node-it.com>
2018-07-25 14:24:52 +0100

    Added SupportsShouldProcess to the Update-MattModules function

--- MattTools/Update-MattModules.ps1
+++ MattTools/Update-MattModules.ps1
@@ -9,17 +9,22 @@ function Update-MattModules {
     Update-MattModules
     #>
 
-    Get-Module -ListAvailable |
-        Where-Object { $_.ModuleBase -like "$env:HOME\*" -and $_.RepositorySourceLocation -like "https://www.myget.org/*" } |
-        Sort-Object -Property Name, Version -Descending |
-        Get-Unique -PipelineVariable Module |
-        ForEach-Object {
-        Find-Module -Name $_.Name -Repository NodePowerShellRepository -OutVariable Repo -ErrorAction SilentlyContinue |
-            Compare-Object -ReferenceObject $_ -Property Name, Version |
-            Where-Object { $_.SideIndicator -eq "=>" } |
-            Select-Object -Property Name, Version,
-        @{ label = 'Repository'; expression = { $Repo.Repository }},
-        @{ label = 'InstalledVersion'; expression = { $Module.Version }}
-    } | ForEach-Object { Install-Module -Name $_.Name -Repository NodePowerShellRepository -Scope CurrentUser -Force }
+    [CmdletBinding(SupportsShouldProcess = $True)]
+    Param ()
 
+
+    if ($PSCmdlet.ShouldProcess("Checked for updates to modules downloaded from NodePowerShellRepository successfully")) {
+        Get-Module -ListAvailable |
+            Where-Object { $_.ModuleBase -like "$env:HOME\*" -and $_.RepositorySourceLocation -like "https://www.myget.org/*" } |
+            Sort-Object -Property Name, Version -Descending |
+            Get-Unique -PipelineVariable Module |
+            ForEach-Object {
+            Find-Module -Name $_.Name -Repository NodePowerShellRepository -OutVariable Repo -ErrorAction SilentlyContinue |
+                Compare-Object -ReferenceObject $_ -Property Name, Version |
+                Where-Object { $_.SideIndicator -eq "=>" } |
+                Select-Object -Property Name, Version,
+            @{ label = 'Repository'; expression = { $Repo.Repository }},
+            @{ label = 'InstalledVersion'; expression = { $Module.Version }}
+        } | ForEach-Object { Install-Module -Name $_.Name -Repository NodePowerShellRepository -Scope CurrentUser -Force }
+    }
 }
