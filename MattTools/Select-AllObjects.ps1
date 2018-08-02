ab7a9b0c87c570767b3431047b47498c827a3019
Matt Collins <Matt.Collins@node-it.com>
2018-08-02 13:57:04 +0100

    Creation of Select-AllObjects.ps1 function

---
+++ MattTools/Select-AllObjects.ps1
@@ -0,0 +1,22 @@
+function Select-AllObjects {
+
+    <#
+    .SYNOPSIS
+    Select all properties
+    .DESCRIPTION
+    Function to select all properties from a command
+    .EXAMPLE
+    Select-AllObjects
+    .EXAMPLE
+    Sel
+    #>
+
+    Param ()
+
+    process {
+        Select-Object *
+    }
+
+}
+
+New-Alias -Name Sel -Value Select-AllObjects
