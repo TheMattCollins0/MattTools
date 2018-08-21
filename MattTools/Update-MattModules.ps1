0d50446bc31aed61cd97d00e5ebf35a3131ede2f
Matt Collins <Matt.Collins@node-it.com>
2018-08-21 15:39:23 +0100

    Improvements to my Update-MattModules function

--- MattTools/Update-MattModules.ps1
+++ MattTools/Update-MattModules.ps1
@@ -4,27 +4,107 @@ function Update-MattModules {
     .SYNOPSIS
     Update Matt modules
     .DESCRIPTION
-    Update modules that are stored in the NodePowerShellRepository
+    Update modules that are stored in PSGallery and NodePowerShellRepository
+    .PARAMETER PSGallery
+    Checks for updates to modules installed from the PSGallery
     .EXAMPLE
     Update-MattModules
+    .EXAMPLE
+    Update-MattModules -PSGallery
+    .NOTES
+    Can also be called by running Update-NodeModules
     #>
 
     [CmdletBinding(SupportsShouldProcess = $True)]
-    Param ()
+    Param (
+        [Parameter(Mandatory = $False)]
+        [switch]$PSGallery)
 
+    if ( !$PSGallery ) {
 
-    if ($PSCmdlet.ShouldProcess("Checked for updates to modules downloaded from NodePowerShellRepository successfully")) {
-        Get-Module -ListAvailable |
-            Where-Object { $_.ModuleBase -like "$env:HOME\*" -and $_.RepositorySourceLocation -like "https://www.myget.org/*" } |
-            Sort-Object -Property Name, Version -Descending |
-            Get-Unique -PipelineVariable Module |
-            ForEach-Object {
-            Find-Module -Name $_.Name -Repository NodePowerShellRepository -OutVariable Repo -ErrorAction SilentlyContinue |
-                Compare-Object -ReferenceObject $_ -Property Name, Version |
-                Where-Object { $_.SideIndicator -eq "=>" } |
-                Select-Object -Property Name, Version,
-            @{ label = 'Repository'; expression = { $Repo.Repository }},
-            @{ label = 'InstalledVersion'; expression = { $Module.Version }}
-        } | ForEach-Object { Install-Module -Name $_.Name -Repository NodePowerShellRepository -Scope CurrentUser -Force }
+        if ($PSCmdlet.ShouldProcess("Checked for updates to modules downloaded from NodePowerShellRepository successfully")) {
+
+            # Create variable containing all modules installed from the NodePowerShellRepository
+            $Modules = @( Get-Module -ListAvailable | Where-Object { $_.ModuleBase -like "$env:HOME\*" -and $_.RepositorySourceLocation -like "https://www.myget.org/*" } | Sort-Object -Property Name, Version -Descending | Get-Unique -PipelineVariable Module )
+
+            Write-Verbose "There are $($Modules.count) modules installed"
+
+            # Create an empty collection to store the modules that need updating
+            $ModulesToUpdate = @()
+
+            ForEach ( $Module in @( $Modules )) {
+                Write-Verbose "Currently selected module is - $($Module.Name)"
+                $SelectModule = Find-Module -Name $($Module.Name) -Repository NodePowerShellRepository | Select-Object Name, Version
+                Write-Verbose "$($SelectModule.Name) module has been found in the NodePowerShellRepository"
+                $ObjectComparison = Compare-Object -ReferenceObject $SelectModule $Module -Property Name, Version | Where-Object { $_.SideIndicator -eq "=>" } | Select-Object -Property Name, Version
+                if ( $ObjectComparison ) {
+                    Write-Information "    An update for $($ObjectComparison.Name) has been found"
+                    $ModuleString = $($ObjectComparison.Name)
+                    $ModulesToUpdate += $ModuleString
+                }
+                else {
+                    Write-Information "    An update for $($Module.Name) has not been found"
+                }
+            }
+
+            if ( $ModulesToUpdate.count -ge 1 ) {
+                Write-Verbose "There are $($ModulesToUpdate.count) modules to be updated"
+
+                # Loop through all modules with updates available and install the latest version
+                ForEach ( $Module in $ModulesToUpdate) {
+                    Write-Information "Currently updating $ModuleName to the latest version"
+                    Install-Module -Name $($Module.Name) -Repository NodePowerShellRepository -Scope CurrentUser -Force
+                }
+            }
+            else {
+                Write-Information "There are no modules that require updates"
+            }
+        }
     }
+
+    elseif ( $PSGallery ) {
+
+        if ($PSCmdlet.ShouldProcess("Checked for updates to modules downloaded from PSGallery successfully")) {
+
+            # Create variable containing all modules installed from the PSGallery
+            $Modules = @( Get-Module -ListAvailable | Where-Object { $_.ModuleBase -like "$env:HOME\*" -and $_.RepositorySourceLocation -like "https://www.powershellgallery" } | Sort-Object -Property Name, Version -Descending | Get-Unique -PipelineVariable Module )
+
+            Write-Verbose "There are $($Modules.count) modules installed"
+
+            # Create an empty collection to store the modules that need updating
+            $ModulesToUpdate = @()
+
+            ForEach ( $Module in @( $Modules )) {
+                Write-Verbose "Currently selected module is - $($Module.Name)"
+                $SelectModule = Find-Module -Name $($Module.Name) -Repository PSGallery | Select-Object Name, Version
+                Write-Verbose "$($SelectModule.Name) module has been found in the PSGallery"
+                $ObjectComparison = Compare-Object -ReferenceObject $SelectModule $Module -Property Name, Version | Where-Object { $_.SideIndicator -eq "=>" } | Select-Object -Property Name, Version
+                if ( $ObjectComparison ) {
+                    Write-Information "    An update for $($ObjectComparison.Name) has been found"
+                    $ModuleString = $($ObjectComparison.Name)
+                    $ModulesToUpdate += $ModuleString
+                }
+                else {
+                    Write-Information "    An update for $($Module.Name) has not been found"
+                }
+            }
+
+            if ( $ModulesToUpdate.count -ge 1 ) {
+                Write-Verbose "There are $($ModulesToUpdate.count) modules to be updated"
+
+                # Loop through all modules with updates available and install the latest version
+                ForEach ( $Module in $ModulesToUpdate) {
+                    Write-Information "Currently updating $ModuleName to the latest version"
+                    Install-Module -Name $($Module.Name) -Repository PSGallery -Scope CurrentUser -Force
+                }
+            }
+            else {
+                Write-Information "There are no modules that require updates"
+            }
+        }
+
+    }
+
 }
+
+New-Alias -Name Update-NodeModules -Value Update-MattModules
