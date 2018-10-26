090e46d8b113fc9ee16f15eae18203f7f829e60b
Matt Collins <Matt.Collins@node-it.com>
2018-10-26 16:14:56 +0100

    Added PropertyType options to the help information

--- MattTools/New-RegistryProperty.ps1
+++ MattTools/New-RegistryProperty.ps1
@@ -11,7 +11,13 @@ function New-RegistryProperty {
     .PARAMETER Value
     Specifies the value of the new registry property
     .PARAMETER PropertyType
-    Specifies the PropertyType of the property
+    Specifies the PropertyType of the property the following property types are available for use:
+    String: Specifies a null-terminated string. Equivalent to REG_SZ.
+    ExpandString: Specifies a null-terminated string that contains unexpanded references to environment variables that are expanded when the value is retrieved. Equivalent to REG_EXPAND_SZ.
+    Binary: Specifies binary data in any form. Equivalent to REG_BINARY.
+    DWord: Specifies a 32-bit binary number. Equivalent to REG_DWORD.
+    MultiString: Specifies an array of null-terminated strings terminated by two null characters. Equivalent to REG_MULTI_SZ.
+    Qword: Specifies a 64-bit binary number. Equivalent to REG_QWORD.
     .EXAMPLE
     New-RegistryProperty -Path "HKLM:\SOFTWARE\NodeIT" -Name Testing -Value "This is the property value" -PropertyType String
     .NOTES
