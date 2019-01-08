a82bf9c6149f68ecd74dbdf4453f891521515daa
Matt Collins <Matt.Collins@node-it.com>
2019-01-08 18:29:54 +0000

    Change to how Export-ModuleMember exports functions and aliases

--- Tools/ManifestUpdate.ps1
+++ Tools/ManifestUpdate.ps1
@@ -21,4 +21,5 @@ $Manifest = Import-PowerShellDataFile $ModulePath
 # Add one to the build of the version number
 [version]$NewVersion = "{0}.{1}.{2}" -f $Version.Major, $Version.Minor, ($Version.Build + 1)
 # Update the manifest file with the new version number and the string of functions to export
-Update-ModuleManifest -Path $ModulePath -ModuleVersion $NewVersion -FunctionsToExport $ExportedFunctions -VariablesToExport "*" -AliasesToExport "*"
+# Update-ModuleManifest -Path $ModulePath -ModuleVersion $NewVersion -FunctionsToExport $ExportedFunctions -VariablesToExport "*" -AliasesToExport "*"
+Update-ModuleManifest -Path $ModulePath -ModuleVersion $NewVersion
