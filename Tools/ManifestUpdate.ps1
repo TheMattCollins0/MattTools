29c6cbcb73bbc7760c59441cfba8d6165efd191e
Matt Collins <Matt.Collins@node-it.com>
2019-01-15 11:57:47 +0000

    Removal of commented lines from the ManifestUpdate function

--- Tools/ManifestUpdate.ps1
+++ Tools/ManifestUpdate.ps1
@@ -1,6 +1,5 @@
 $ModuleName = $env:BUILD_DEFINITIONNAME
 $FunctionPath = ".\Source\Public\"
-# $ModulePath = ".\" + $ModuleName + "\" + $ModuleName + ".psd1"
 $ModulePath = ".\Source\" + $ModuleName + ".psd1"
 
 # Create array containing all the functions
@@ -22,5 +21,3 @@ $Manifest = Import-PowerShellDataFile $ModulePath
 [version]$NewVersion = "{0}.{1}.{2}" -f $Version.Major, $Version.Minor, ($Version.Build + 1)
 # Update the manifest file with the new version number and the string of functions to export
 Update-ModuleManifest -Path $ModulePath -ModuleVersion $NewVersion -FunctionsToExport $ExportedFunctions -VariablesToExport "*" -AliasesToExport "*"
-# Update-ModuleManifest -Path $ModulePath -ModuleVersion $NewVersion -FunctionsToExport $ExportedFunctions -VariablesToExport "*" -AliasesToExport 'Update-NodeModules', 'C', 'PH', 'Output', 'Out', 'In', 'Input', 'Gh', 'GitHub'
-# Update-ModuleManifest -Path $ModulePath -ModuleVersion $NewVersion
