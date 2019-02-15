d75d1b682c8dc6ab18707543d4100c869d4a2dd4
Matt Collins <Matt.Collins@node-it.com>
2019-02-15 17:40:27 +0000

    Change to the Add-NodeRepository function

--- Source/Public/Add-NodeRepository.ps1
+++ Source/Public/Add-NodeRepository.ps1
@@ -1,4 +1,4 @@
-function Add-AzureDevOpsRepository {
+function Add-NodeRepository {
 
     #Requires -Modules BetterCredentials
 
@@ -14,7 +14,7 @@ function Add-AzureDevOpsRepository {
     .PARAMETER FeedName
     This is the name of the Azure Artifacts feed for the repository
     .EXAMPLE
-    Add-AzureDevOpsRepository -RepositoryName TestRepository -Username UsernameHere -FeedName FeedName -Verbose
+    Add-NodeRepository -RepositoryName TestRepository -Username UsernameHere -FeedName FeedName -Verbose
     .NOTES
     This function also supports the -Verbose parameter to show more detailed console output
     #>
