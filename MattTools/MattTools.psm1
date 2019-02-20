3fe4369d18417d5814679c25d0ef1b5de297f809
TheMattCollins0 <matt.collins@node-it.com>
2019-02-20 11:20:32 +0000

    Updated Version Number ***NO_CI***

--- MattTools/MattTools.psm1
+++ MattTools/MattTools.psm1
@@ -1,3 +1,32 @@
+function Get-NugetExe {
+
+    # Function to download the NuGet executable to C:\ProgramData\Node\Nuget\nuget.exe
+
+    $nugetPath = "C:\ProgramData\Node\Nuget"
+    if (!(Test-Path -Path $nugetPath)) {
+        Write-Verbose "Creating directory $nugetPath" -Verbose
+        New-Item -Path $nugetPath -ItemType Directory
+    }
+    Write-Verbose "Working Folder : $nugetPath"
+    $NugetExe = "$nugetPath\nuget.exe"
+    if (-not (Test-Path $NugetExe)) {
+        Write-Verbose "Cannot find nuget at $NugetExe" -Verbose
+        $NuGetInstallUri = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+        $sourceNugetExe = $NuGetInstallUri
+        Write-Verbose "$sourceNugetExe -OutFile $NugetExe" -Verbose
+
+        Invoke-WebRequest $sourceNugetExe -OutFile $NugetExe
+        if (-not (Test-Path $NugetExe)) {
+            Throw "Nuget download hasn't worked."
+        }
+        Else {Write-Verbose "Nuget Downloaded!" -Verbose}
+    }
+    Write-Verbose "Add $nugetPath as %PATH%"
+    $pathenv = [System.Environment]::GetEnvironmentVariable("path")
+    $pathenv = $pathenv + ";" + $nugetPath
+    [System.Environment]::SetEnvironmentVariable("path", $pathenv)
+
+}
 function Add-ArtifactsCredential {
 
     #Requires -Modules BetterCredentials
@@ -83,6 +112,9 @@ function Add-NodeRepository {
     )
 
     begin {
+
+        # Download the Nuget executable using the Get-NugetExe function
+        Get-NugetExe
 
         # Creation of the RepositoryURL variable from the FeedName parameter
         $RepositoryURL = "https://pkgs.dev.azure.com/MattNodeIT/_packaging/" + $FeedName + "/nuget/v2"
