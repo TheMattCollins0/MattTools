75ec3ef5f5477761d9343f60cdd35a2a77b4ef3a
Matt Collins <Matt.Collins@node-it.com>
2019-03-27 10:39:05 +0000

    Creation of the Start-Ping function

---
+++ Source/Public/Start-Ping.ps1
@@ -0,0 +1,28 @@
+function Start-Ping {
+
+    <#
+    .SYNOPSIS
+    Run Ping with -t specified as a default argument
+    .DESCRIPTION
+    This function wraps around Ping supplying the-t argument to always ping non-stop
+    .PARAMETER Address
+    The -Address parameter is for supplying the IP address or hostname you wish to ping
+    .EXAMPLE
+    Start-Ping test.domain.com
+    .EXAMPLE
+    P 1.1.1.1
+    .NOTES
+    Function supports the alias P
+    #>
+
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [string]$Address
+    )
+
+    ping $Address -t
+
+}
+
+New-Alias -Name P -Value Start-Ping
