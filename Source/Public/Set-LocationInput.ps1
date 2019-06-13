a590dceba807dc946a1b1d42934f9df2a1cc53da
Matt Collins <Matt.Collins@node-it.com>
2019-06-13 18:08:32 +0100

    Removal of the In alias from the Set-LocationInput function

--- Source/Public/Set-LocationInput.ps1
+++ Source/Public/Set-LocationInput.ps1
@@ -25,7 +25,7 @@ function Set-LocationInput {
 
     )
 
-    if ($PSCmdlet.ShouldProcess("Change of location to the $InputPath path successful")) {     
+    if ($PSCmdlet.ShouldProcess("Change of location to the $InputPath path successful")) {
         # Test if the $InputPath path is valid
         if (Test-Path "$InputPath") {
             # Set console location to the InputPath drive
@@ -38,5 +38,4 @@ function Set-LocationInput {
     }
 }
 
-New-Alias -Name In -Value Set-LocationInput
 New-Alias -Name Input -Value Set-LocationInput
